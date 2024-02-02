1d180466917615245b827b6e1fd43655a66a5227
Akash044 <akash.cse.ruet@gmail.com>
2024-02-02 15:17:11 +0600

    basic-math-operators-javascript

---
+++ javascript-challenges/jschallenger/basic-math-operators-javascript.js
@@ -0,0 +1,4 @@
+//https://jschallenger.com/javascript-practice/javascript-fundamentals/basic-math-operators-javascript
+function myFunction(a, b, c, d, e, f) {
+  return Math.pow(((a + b - c) * d) / e, f);
+}
