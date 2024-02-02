a72197e875f6e4637cb367e6cdca85446ef99f9a
Akash044 <akash.cse.ruet@gmail.com>
2024-02-02 16:25:20 +0600

    round-2-decimal-digits

---
+++ javascript-challenges/jschallenger/round-2-decimal-digits.js
@@ -0,0 +1,6 @@
+//https://jschallenger.com/javascript-practice/javascript-fundamentals/round-2-decimal-digits
+function myFunction(a) {
+  return Math.round(a * 100) / 100;
+}
+
+console.log(myFunction(3.136));
