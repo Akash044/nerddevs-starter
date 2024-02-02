44906ee3b8ac273fe07a3775922629620baa7333
Akash044 <akash.cse.ruet@gmail.com>
2024-02-02 16:19:53 +0600

    multiplication-division-comparison-operators

---
+++ javascript-challenges/jschallenger/multiplication-division-comparison-operators.js
@@ -0,0 +1,5 @@
+//https://jschallenger.com/javascript-practice/javascript-fundamentals/multiplication-division-comparison-operators
+
+function myFunction(a, b) {
+  return a < b ? a / b : a * b;
+}
