51e9d9213a46a342e9ad0201bfd060a97ba197b7
Akash044 <akash.cse.ruet@gmail.com>
2024-02-02 14:59:06 +0600

    Get first n characters of string

---
+++ javascript-challenges/jschallenger/Get first n characters of string.js
@@ -0,0 +1,4 @@
+//https://jschallenger.com/javascript-practice/javascript-fundamentals/get-first-characters-string-javascript
+function myFunction(a) {
+  return a.split("").splice(0, 3, "").join("");
+}
