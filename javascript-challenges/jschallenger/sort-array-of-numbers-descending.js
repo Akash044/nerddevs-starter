32affff81b77a37ae75eacc36d263d2eb6dc6151
Akash044 <akash.cse.ruet@gmail.com>
2024-02-02 17:00:33 +0600

    sort-array-of-numbers-descending

---
+++ javascript-challenges/jschallenger/sort-array-of-numbers-descending.js
@@ -0,0 +1,6 @@
+//https://jschallenger.com/javascript-practice/javascript-arrays/sort-array-of-numbers
+function myFunction(arr) {
+  return arr.sort((a, b) => b - a);
+}
+
+console.log(myFunction([1, 3, 2]));
