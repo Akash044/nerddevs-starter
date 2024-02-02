c06e769888c2003cc093149aafb884f84e4a290b
Akash044 <akash.cse.ruet@gmail.com>
2024-02-02 16:04:42 +0600

    check-if-string-contains-string

---
+++ javascript-challenges/jschallenger/check-if-string-contains-string.js
@@ -0,0 +1,5 @@
+//https://jschallenger.com/javascript-practice/javascript-fundamentals/check-if-string-contains-string
+function myFunction(a, b) {
+  return a.includes(b) ? b + a : a + b;
+}
+console.log(myFunction("Java", "script"));
