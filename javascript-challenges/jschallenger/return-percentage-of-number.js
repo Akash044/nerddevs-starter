afc25efe6cc8608f64d49157a5b3479a044beb2a
Akash044 <akash.cse.ruet@gmail.com>
2024-02-02 15:15:08 +0600

    return-percentage-of-number

---
+++ javascript-challenges/jschallenger/return-percentage-of-number.js
@@ -0,0 +1,7 @@
+//https://jschallenger.com/javascript-practice/javascript-fundamentals/return-percentage-of-number
+
+function myFunction(a, b) {
+  return (a / 100) * b;
+}
+
+console.log(myFunction(500, 25));
