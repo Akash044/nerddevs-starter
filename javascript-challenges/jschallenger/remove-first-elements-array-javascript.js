e7d61e51a1ba23549073e90bfeda11654f3cdb8b
Akash044 <akash.cse.ruet@gmail.com>
2024-02-02 16:45:52 +0600

    remove-first-elements-array-javascript

---
+++ javascript-challenges/jschallenger/remove-first-elements-array-javascript.js
@@ -0,0 +1,6 @@
+//https://jschallenger.com/javascript-practice/javascript-arrays/remove-first-elements-array-javascript
+function myFunction(a) {
+  return a.slice(3);
+}
+
+console.log(myFunction([1, 2, 3, 4]));
