6658b19966578f149f213846bca8261ba2ac1e95
Akash044 <akash.cse.ruet@gmail.com>
2024-02-02 15:03:12 +0600

    index-of-substring-in-string

---
+++ javascript-challenges/jschallenger/index-of-substring-in-string.js
@@ -0,0 +1,7 @@
+//https://jschallenger.com/javascript-practice/javascript-fundamentals/index-of-substring-in-string
+
+function myFunction(a) {
+  return a.indexOf("is");
+}
+
+console.log(myFunction("paris"));
