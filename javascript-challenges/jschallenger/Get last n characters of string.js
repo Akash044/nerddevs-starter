c0623ed8dded9d6ceaf47d9067f774616e60974d
Akash044 <akash.cse.ruet@gmail.com>
2024-02-02 14:56:55 +0600

    Get last n characters of string

---
+++ javascript-challenges/jschallenger/Get last n characters of string.js
@@ -0,0 +1,6 @@
+//https://jschallenger.com/javascript-practice/javascript-fundamentals/get-last-characters-string-javascript
+function myFunction(str) {
+  return str.slice(str.length - 3);
+}
+
+console.log(myFunction("abcdefg"));
