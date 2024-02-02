5038a6593e85de85dd065f715ed265d12fbf3a7e
Akash044 <akash.cse.ruet@gmail.com>
2024-02-02 15:05:23 +0600

    extract-first-half-string-javascript

---
+++ javascript-challenges/jschallenger/extract-first-half-string-javascript.js
@@ -0,0 +1,7 @@
+//https://jschallenger.com/javascript-practice/javascript-fundamentals/extract-first-half-string-javascript
+
+function myFunction(a) {
+  return a.slice(0, a.length / 2);
+}
+
+console.log(myFunction("1234"));
