76aa157b0d08b42e851b94e7f64a89d5a64d964f
Akash044 <akash.cse.ruet@gmail.com>
2024-02-05 12:36:49 +0600

    create-javascript-set

---
+++ javascript-challenges/jschallenger/create-javascript-set.js
@@ -0,0 +1,6 @@
+//https://jschallenger.com/javascript-practice/javascript-sets/create-javascript-set
+function myFunction(a, b, c) {
+  return new Set([a, b, c]);
+}
+
+console.log(myFunction(1, "b", 3));
